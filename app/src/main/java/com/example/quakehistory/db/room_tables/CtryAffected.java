b97b2e2e59c2e0c01fb15824dcdc29a4b046147e
JoseLaptop <josemarquezmontoro@hotmail.com>
2023-02-24 11:34:42 +0100

    room table class implemented

--- app/src/main/java/com/example/quakehistory/db/room_tables/CtryAffected.java
+++ app/src/main/java/com/example/quakehistory/db/room_tables/CtryAffected.java
@@ -1,4 +1,24 @@
 package com.example.quakehistory.db.room_tables;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+
+@Entity (
+        foreignKeys = {
+                @ForeignKey(
+                        entity = EarthQuake.class,
+                        parentColumns = "date",
+                        childColumns = "date",
+                        onDelete = ForeignKey.CASCADE
+                )
+        },
+        tableName = "ctry_affected"
+)
 public class CtryAffected {
+    @PrimaryKey
+    public String date;
+    @PrimaryKey
+    public String country;
 }
--- app/src/main/java/com/example/quakehistory/db/room_tables/EarthQuake.java
+++ app/src/main/java/com/example/quakehistory/db/room_tables/EarthQuake.java
@@ -1,4 +1,30 @@
 package com.example.quakehistory.db.room_tables;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Index;
+import androidx.room.PrimaryKey;
+
+@Entity (
+        indices = {@Index(value = {"device_name"}, unique = true)},
+        tableName = "earth_quake"
+)
 public class EarthQuake {
+    @PrimaryKey
+    public String date;
+
+    @ColumnInfo(name = "device_name")
+    public String deviceName;
+
+    @ColumnInfo(name = "magnitude")
+    public float magnitude;
+
+    @ColumnInfo(name = "coordinates")
+    public String coordinates;
+
+    @ColumnInfo(name = "location")
+    public String location;
+
+    @ColumnInfo(name = "death_toll")
+    public int deathToll;
 }
