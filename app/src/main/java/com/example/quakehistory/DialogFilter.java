bd74f39db6794443321dadddb2515fa0db67a883
Jose Marquez <josemarquezmontoro2@gmail.com>
2023-02-25 00:43:49 +0100

    dialog implemented and showed into application

--- app/src/main/java/com/example/quakehistory/DialogFilter.java
+++ app/src/main/java/com/example/quakehistory/DialogFilter.java
@@ -1,19 +1,13 @@
 package com.example.quakehistory;
-
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
-
-import androidx.annotation.Nullable;
-import androidx.fragment.app.DialogFragment;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-
+import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
 public class DialogFilter extends DialogFragment {
@@ -22,7 +16,8 @@ public class DialogFilter extends DialogFragment {
     private EditText etMag;
     private Spinner spnCtry;
 
-    public Dialog OnCreateDialog(@Nullable Bundle savedInstanceState) {
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         View v = getActivity().getLayoutInflater().inflate(R.layout.dialog_filter, null);
 
         spnMag = v.findViewById(R.id.spnMag);
@@ -59,15 +54,17 @@ public class DialogFilter extends DialogFragment {
         return ad;
     }
 
+    @Override
     public void onAttach(Context context) {
         super.onAttach(context);
         try {
             mListener = (OnDialogListener) context;
         } catch (ClassCastException e) {
-            throw new ClassCastException(context.toString() + getString(R.string.implement_OnDialogList));
+            throw new ClassCastException(context + getString(R.string.implement_OnDialogList));
         }
     }
 
+    @Override
     public void onDetach() {
         if (mListener != null) {
             mListener = null;
--- app/src/main/java/com/example/quakehistory/MainActivity.java
+++ app/src/main/java/com/example/quakehistory/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.quakehistory;
 
 import android.os.Bundle;
+import android.widget.Button;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements OnDialogListener{
+    private Button btnShow;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initButton();
     }
+
+    private void initButton() {
+        btnShow = findViewById(R.id.btnShow);
+        btnShow.setOnClickListener(v -> {
+            DialogFilter dialogFilter = new DialogFilter();
+            dialogFilter.setCancelable(false);
+            dialogFilter.show(getSupportFragmentManager(), "dialogFilter");
+        });
+        }
+
+    @Override
+    public void onDialogPositiveClick(String mag, String magValue, String ctry) {
+    }
+
 }
